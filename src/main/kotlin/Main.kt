bd128f2731061829baa59f25a6bd58b471b7dac3
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2022-06-12 18:13:34 -0400

    new class Aquarium added and initialized in main class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import example.myapp.Aquarium
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun buildAquarium() {
+    val myAquarium = Aquarium()
+    myAquarium.printSize()
+}
+
+fun main() {
+    buildAquarium()
 }
