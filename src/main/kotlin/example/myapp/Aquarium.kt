03f6f7c2d60c029f034881c1e357661a3af6f85e
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2022-06-12 20:30:23 -0400

    initializer blocks added

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,6 +1,12 @@
 package example.myapp
 
 class Aquarium(var width: Int = 20, var height: Int = 40, var length: Int = 100) {
+    init {
+        println("Aquarium initializing");
+    }
+    init {
+        println("volume: ${(width * height * length) / 1000} l")
+    }
     fun printSize() {
         println(
             "Width: $width Height: $height Length: $length",
