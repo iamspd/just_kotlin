9b2bd292f7efacd80b5c9a42cfe585389db346f4
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2022-06-12 20:41:49 -0400

    second constructor created and referenced from Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,9 @@ import example.myapp.Aquarium
 fun buildAquarium() {
     val myAquarium = Aquarium()
     myAquarium.printSize()
+
+    val myAquarium1 = Aquarium(numberOfFish = 29)
+    myAquarium1.printSize()
 }
 
 fun main() {
--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -7,6 +7,12 @@ class Aquarium(var width: Int = 20, var height: Int = 40, var length: Int = 100)
     init {
         println("volume: ${(width * height * length) / 1000} l")
     }
+
+    constructor(numberOfFish: Int) : this() {
+        // 2000 cm*3 per fish + extra room so water doesn't spill
+        val tank = numberOfFish * 2000 * 1.1
+        height = (tank / (width * length)).toInt()
+    }
     fun printSize() {
         println(
             "Width: $width Height: $height Length: $length",
